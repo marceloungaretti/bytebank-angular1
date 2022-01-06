a646f7181e144b95cf90fdc7987d27ddc783f156
Marcelo <marcelo.ungaretti@gmail.com>
2022-01-06 16:25:42 -0300

    fim do capitulo 6

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,5 +1,6 @@
 import { TransferenciaService } from './../services/transferencia.service';
 import { Component, OnInit } from '@angular/core';
+import { Transferencia } from 'models/transferencia.model';
 
 @Component({
   selector: 'app-extrato',
@@ -11,7 +12,10 @@ export class ExtratoComponent implements OnInit {
 
   constructor(private service: TransferenciaService) {}
 
-  ngOnInit(): void {
-    this.transferencias = this.service.transferencias;
+  ngOnInit() {
+    this.service.todas().subscribe((transferencias: Transferencia[]) => {
+      console.table(transferencias);
+      this.transferencias = transferencias;
+    });
   }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,27 +1,37 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Transferencia } from 'models/transferencia.model';
+import { TransferenciaService } from '../services/transferencia.service';
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
-
+export class NovaTransferenciaComponent {
   @Output() aoTransferir = new EventEmitter();
 
   valor: number;
   destino: number;
 
-  transferir(){
+  constructor(private service: TransferenciaService) {}
+
+  transferir() {
     console.log('Solicitada nova transferência');
 
-    const valorEmitir = {valor: this.valor, destino: this.destino};
-    this.aoTransferir.emit(valorEmitir);
-
-    this.limparCampos();
+    const valorEmitir: Transferencia = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limparCampos();
+      },
+      (error) => console.error(error)
+    );
   }
 
-  limparCampos(){
+  limparCampos() {
     this.valor = 0;
     this.destino = 0;
   }
