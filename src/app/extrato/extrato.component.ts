827bdc6a463fe8c237bcfe862f60c8245542538a
Marcelo <marcelo.ungaretti@gmail.com>
2022-01-06 12:22:54 -0300

    fim do capitulo 5

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
   templateUrl: './extrato.component.html',
-  styleUrls: ['./extrato.component.scss']
+  styleUrls: ['./extrato.component.scss'],
 })
 export class ExtratoComponent implements OnInit {
+  transferencias: any[];
 
-  @Input() transferencias: any[];
-
-  constructor() { }
+  constructor(private service: TransferenciaService) {}
 
   ngOnInit(): void {
+    this.transferencias = this.service.transferencias;
   }
-
 }
